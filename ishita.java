b9ac9426f0032c556da52660bdb25c1e3726dfad
Nitin shishodia <nitinshishodia123@gmail,com>
2023-02-13 17:25:44 +0530

    ishita.java modified

--- ishita.java
+++ ishita.java
@@ -1,5 +1,8 @@
  public class ishita{
 public status void main(String[] args){
 System.out.println("hallo world");
+public status void main(String[] args){
+System.out.println("hallo world");
+}
 }
 }
